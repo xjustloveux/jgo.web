4bb12ce8ca15cdbf8e83a63858b1c8b7525f7a05
JaJa <JaJa@ares.com.tw>
2022-12-01 22:31:32 +0800

    fix .gitignore

---
+++ middleware/log/sql.go
@@ -0,0 +1,13 @@
+// Copyright 2022 JaJa All rights reserved.
+// Use of this source code is governed by a MIT-style.
+// license that can be found in the LICENSE file.
+
+package log
+
+import (
+	"github.com/xjustloveux/jgo/jlog"
+)
+
+func SqlLog(args ...interface{}) {
+	jlog.Info(args...)
+}
